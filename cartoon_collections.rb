8d1c93bf6fc4dff6407392948bea92801985c5cb
William Lathrop <wlathrop10@gmail.com>
2018-08-29 16:22:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,6 +1,8 @@
 def roll_call_dwarves# code an argument here
-  # Your code here
-  
+    dwarves.each_with_index do |name, index|
+    index += 1
+    puts "#{index}. #{name}"
+  end
 end
 
 def summon_captain_planet# code an argument here
