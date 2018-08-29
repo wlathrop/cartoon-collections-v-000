302f0fe95c3bac2af1a4f00de24b83a41fbfd30a
William Lathrop <wlathrop10@gmail.com>
2018-08-29 16:26:54 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,8 +5,10 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-def summon_captain_planet code an argument here
-  # Your code here
+f summon_captain_planet(planeteer_calls)
+  planeteer_calls.collect do |call|
+    call.capitalize << "!"
+  end
 end
 
 def long_planeteer_calls# code an argument here
