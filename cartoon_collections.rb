2c90a35e387aab541c1ffd9d2b12e16ff3e45ff9
William Lathrop <wlathrop10@gmail.com>
2018-08-29 16:27:16 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -5,7 +5,7 @@ def roll_call_dwarves(dwarves)
   end
 end
 
-f summon_captain_planet(planeteer_calls)
+def summon_captain_planet(planeteer_calls)
   planeteer_calls.collect do |call|
     call.capitalize << "!"
   end
